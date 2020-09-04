674cc6393022008746f2a809451c87f7b3c9c884
Ben Matthews <ben@montfort.io>
2020-09-04 21:50:37 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://zen-thompson-ae1e01.netlify.app`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
