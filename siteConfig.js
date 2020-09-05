15742c2b857161dd82fbeff96b98ea771d30c6f4
Ben Matthews <ben@montfort.io>
2020-09-05 08:21:37 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,22 +1,22 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://zen-thompson-ae1e01.netlify.app`,
+    siteUrl: `https://blog.goodhere.org`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
 
     // Initial number fetched, scrolling lazy loads posts one by one
     // If infinite scroll is disabled: maximum number of post shown per page
-    postsPerPage: 3,
+    postsPerPage: 9,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Good Here Blog`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `Discover social impact startups, connect with the social impact community, and find social impact funding.`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Good Here`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
